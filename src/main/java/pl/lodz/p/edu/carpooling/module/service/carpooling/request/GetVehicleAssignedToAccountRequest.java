eca4b3cf9db3beb8b93db525e3e066e8ef5c0d7f
MaciejBartos <maciejbartos@outlook.com>
2021-01-31 15:49:49 +0100

    Remove unnecessary request

--- src/main/java/pl/lodz/p/edu/carpooling/module/service/carpooling/request/GetVehicleAssignedToAccountRequest.java
+++
@@ -1,9 +0,0 @@
-package pl.lodz.p.edu.carpooling.module.service.carpooling.request;
-
-import lombok.Data;
-
-@Data
-public class GetVehicleAssignedToAccountRequest {
-
-    private String accountLogin;
-}
